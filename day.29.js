5a80442da39d8653e56787dab851f0eba4afec56
zainulla R <152606739+Zainulla1@users.noreply.github.com>
2024-07-09 21:56:28 +0530

    Add files via upload

---
+++ day.29.js
@@ -0,0 +1,8 @@
+//Qs
+
+//Find a string and Number by using setTimeout method in JS
+console.log('start');
+setTimeout(() => {console.log("string1"); }, "3000");
+setTimeout(( ) => {console.log("string2");},0);
+setTimeout(() => {console.log("Number");}, 5000);
+console.log('end');
